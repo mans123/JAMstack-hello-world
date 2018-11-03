5db99094d0d52406e881810a79ea56cf329427fe
Md Mansoor Ahmed <ahmed.ans123@gmail.com>
2018-11-03 19:45:07 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const IndexPage = () => (
+  <div>
+    <h1>Md Mansoor Ahmed</h1>
+    <h2>Aspiring Front-End Developer</h2>
+  </div>
+);
+
+export default IndexPage;
